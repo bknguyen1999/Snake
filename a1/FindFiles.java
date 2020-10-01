f6f18c04c7fc310bec181638c12a824289ccde35
brandon <bknguyen1999@gmail.com>
2020-09-30 21:35:29 -0400

    finished findfiles and edited the readme

--- a1/FindFiles.java
+++ a1/FindFiles.java
@@ -3,6 +3,8 @@ import java.io.*;
 import java.util.Vector;
 import java.util.Arrays;
 import java.util.stream.*;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 
 
@@ -12,17 +14,100 @@ public class FindFiles {
 
 
     public static void help(){
-        System.out.println("HELP");
+        System.out.println("Usage: java FindFiles filetofind [-option arg]");
+        System.out.println("-help                     :: print out a help page and exit the program.");
+        System.out.println("-r                        :: execute the command recursively in subfiles.");
+        System.out.println("-reg                      :: treat `filetofind` as a regular expression when searching.");
+        System.out.println("-dir [directory]          :: find files starting in the specified directory.");
+        System.out.println("-ext [ext1,ext2,...]      :: find files matching [filetofind] with extensions [ext1, ext2,...].");
     }
 
     public static boolean verify_input(String args[]){
-        return true;
+        if(args.length == 0){
+            System.out.println("ERROR: FindFiles requires arguments. See -help menu for more details.");
+            help();
+            return false;
+        }
+        for(int i=1; i < args.length; i++){ // first check to see if there's a help flag
+            if (args[i].equals("-help")){
+                help();
+                return false;
+            }
+        }
+        if (args[0].equals("-r") || args[0].equals("-ext") || args[0].equals("-reg") || args[0].equals("-dir")){
+            System.out.println("ERROR: First argument must be the file name to look for");
+            help();
+            return false;
+        }
+        if (args.length == 1){
+            return true;
+        }
+        else{
+            boolean is_option = true;
+            boolean is_dir = false; // we need this because we need to check the arg after the -dir flag
+            boolean is_ext = false; // we need this because we need to check the arg after the -ext flag
+            for (int i = 1; i < args.length; i++){
+                if (is_option){
+                    if (!(args[i].equals("-r") || args[i].equals("-ext") || args[i].equals("-reg") || args[i].equals("-dir"))){
+                        System.out.println("ERROR: " + args[i] + " is not a valid option. Please refer to the -help menu for valid options:");
+                        help();
+                        return false;
+                    }
+                    if (args[i].equals("-ext") || args[i].equals("-dir")){
+                        is_option = false; // turn it off because next arg should be the specified extensions
+                        if (args[i].equals("-ext")){
+                            is_ext = true;
+                        }
+                        else{
+                            is_dir = true;
+                        }
+                        if (i == args.length - 1){
+                            System.out.println("ERROR: " + args[i] + " requires arguments to be specified but none were found");
+                            help();
+                            return false;
+                        }
+                    }
+                    else if (args[i].equals("-reg")){
+                        try{
+                            String pattern = args[0];
+                            Pattern.compile(pattern);
+                        }
+                        catch(PatternSyntaxException e){
+                            System.out.println("ERROR: Invalid regex expression");
+                            help();
+                            return false;
+                        }
+                    }
+                }
+                else{ // expecting parameters for the options
+                    if (args[i].equals("-r") || args[i].equals("-ext") || args[i].equals("-reg") || args[i].equals("-dir")){
+                        System.out.println("ERROR: " + args[i-1] + " requires arguments to be specified but " + args[i] + " was found");
+                        help();
+                        return false;
+                    }
+                    if (is_dir){
+                        String startingdir = new File("").getAbsolutePath();
+                        File d = new File(startingdir +"/" + args[i]);
+                        if (!d.isDirectory()){
+                            System.out.println("ERROR: " + args[i] + " is not a valid directory");
+                            help();
+                            return false;
+                        }
+                        is_dir = false;
+                    }
+                    is_ext = false;
+                    is_option = true;
+                }
+            }
+            return true;
+        }
     }
 
-    public static boolean check_extensions(String file, String[] extensions){
+    public static boolean check_extensions_reg(String file, String[] extensions){
         File f = new File(file);
         return (Arrays.stream(extensions).anyMatch(entry -> file.endsWith(entry))) && f.isFile();
     }
+
 
     public static boolean is_directory(String s){
         File f = new File(s);
@@ -30,9 +115,9 @@ public class FindFiles {
     }
 
     public static void find_file(String filetofind, String directory, HashMap<String, String> options, String[] extensions){
-        // looks for file in current directory and does a dfs search in the subdirectories if file isnt found
-        System.out.println("=============== Looking for file(s) in directory: " + directory + " ===============");
+        // looks for file in current directory and does a bfs search in the subdirectories if file isnt found
         File dir = new File(directory);
+        System.out.println("=============== Looking for file(s) in directory: " + dir.getAbsolutePath() + " ===============");
         String[] list_files_dir = dir.list();
 
         Vector<String> copy = new Vector<String>(Arrays.asList(list_files_dir));
@@ -41,18 +126,31 @@ public class FindFiles {
         }
 
         if (options.containsKey("-ext")){
-            copy.removeIf(f -> !check_extensions(directory + "/" + f, extensions));
-            if (!options.containsKey("-reg")){
-                copy.removeIf(f -> !(f.equals(filetofind)));
+            if (options.containsKey("-reg")){
+                copy.removeIf(f -> !check_extensions_reg(directory + "/" + f, extensions));
+            }
+            else{
+                copy.removeAllElements();
+                for (int i = 0; i < extensions.length; i++){
+                    File f = new File(directory + "/" + filetofind + extensions[i]);
+                    if (f.isFile()){
+                        copy.add(filetofind + extensions[i]);
+                    }
+                }
             }
         }
         if (options.containsKey(("-reg"))){
             copy.removeIf(f -> !f.matches(filetofind));
         }
 
-        for (int i = 0; i < copy.size(); i++) {
-            File f = new File(copy.get(i));
-            System.out.println(f.getAbsolutePath());
+        if (copy.size() > 0){
+            for (int i = 0; i < copy.size(); i++) {
+                File f = new File(directory + "/" + copy.get(i));
+                System.out.println(f.getAbsolutePath());
+            }
+        }
+        else{
+            System.out.println("No files found in this directory");
         }
         // by now, you should have already gotten all files in the current directory so just do a dfs on the subdirectories
         if (options.containsKey("-r")) {
@@ -66,22 +164,12 @@ public class FindFiles {
 
 
     public static void main(String args[]){
-        String filetofind = args[0];
-        System.out.println("filetofind:" + filetofind);
-        for(int i=1; i < args.length; i++){ // first check to see if there's a help flag
-            if (args[i].equals("-help")){
-                help();
-                return;
-                //System.out.println("help");
-            }
-        }
         if (verify_input(args) == false){ // verify that the command line inputs are valid
-            help();
             return;
-            //System.out.println("invalid input");
         }
+        String filetofind = args[0];
         HashMap<String, String> options = new HashMap<String, String>();
-        String startingDir = "."; // current directory
+        String startingDir = new File("").getAbsolutePath();
         String[] extensions = {};
         for(int i = 1; i < args.length; i++) {
             if (args[i].equals("-r")) {
@@ -89,8 +177,14 @@ public class FindFiles {
             } else if (args[i].equals("-reg")) {
                 options.put("-reg", filetofind);
             } else if (args[i].equals("-dir")) {
-                options.put("-dir", args[i + 1]);
-                startingDir = args[i + 1];
+                String chosen_dir = args[i+1];
+                try{
+                    startingDir = new File(startingDir + "/" + chosen_dir).getCanonicalPath();
+                    options.put("-dir", startingDir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             } else if (args[i].equals("-ext")) {
                 options.put("-ext", args[i + 1]);
                 String val = options.get("-ext");
